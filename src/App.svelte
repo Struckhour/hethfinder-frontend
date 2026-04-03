2187d4b77a19dcf418f82d6d466c79701b54e509
Luke McLean <lukecmclean@gmail.com>
2026-04-03 16:27:18 -0400

    adds new styling and borders

--- src/App.svelte
+++ src/App.svelte
@@ -40,25 +40,28 @@
 <main
   class="p-8 font-sans flex h-screen justify-center bg-no-repeat bg-center bg-cover"
   style="background-image: url('{backWhite}');"
-  >
-  <div class="grid grid-cols-[minmax(50px,auto)_1fr_minmax(50px,auto)] items-start gap-4 max-w-[1000px] max-auto">
+>
+  <div class="grid grid-cols-1 md:grid-cols-[minmax(50px,auto)_1fr_minmax(50px,auto)] items-start gap-4 max-w-[1000px] mx-auto">
     
     <!-- Left border image -->
-    <div>
+    <div class="hidden md:block">
       <img src={leftBorder} alt="Left border" class="h-full object-contain max-h-screen" />
     </div>
 
     <!-- Main content -->
-    <div
-      class="text-center"
-    >
+    <div class="text-center">
       <div class="my-4">
         <h1 class="text-6xl font-bold mb-1 tracking-widest" style="font-family: 'Alumni Sans Pinstripe', sans-serif;">HethFinder</h1>
         <h4>A tool for analyzing Hermit Thrush recordings</h4>
       </div>
 
       <div class="w-full flex flex-col">
-        <input type="file" accept=".wav" on:change="{(e) => file = e.target.files?.[0] ?? null}" class="mb-4 mx-auto border border-red-500" />
+        <input
+          type="file"
+          accept=".wav"
+          on:change="{(e) => file = e.target.files?.[0] ?? null}"
+          class="mb-4 mx-auto border border-red-500"
+        />
         
         <button
           on:click="{uploadFile}"
@@ -69,18 +72,19 @@
         </button>
       </div>
 
-      {#if spectrogramUrl}
-      <div class="mt-4">
-        <h2 class="text-lg font-bold mb-2">Spectrogram</h2>
-        <img src={spectrogramUrl} alt="Spectrogram" class="border border-blue object-cover w-full" />
-      </div>
-      {/if}
+
     </div>
 
     <!-- Right border image -->
-    <div>
+    <div class="hidden md:block">
       <img src={rightBorder} alt="Right border" class="h-full object-contain max-h-screen" />
     </div>
 
+    {#if spectrogramUrl}
+    <div class="my-4 py-4 col-span-1 md:col-span-3 w-full">
+      <h2 class="text-lg font-bold mb-2">{file ? file.name : "Spectrogram"}</h2>
+      <img src={spectrogramUrl} alt="Spectrogram" class="border-t-4 border-b-4 border-black object-cover w-full" />
+    </div>
+    {/if}
   </div>
 </main>
