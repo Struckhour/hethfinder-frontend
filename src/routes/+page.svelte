21ca01f5ae8090fa0b3fbb84eaff7e1760ec1514
Luke McLean <lukecmclean@gmail.com>
2026-04-12 10:08:38 -0400

    removes sveltekit component structure

--- src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import Spectrogram from '$lib/components/Spectrogram.svelte';
-</script>
-
-<main class="p-8">
-  <h1 class="text-2xl font-bold mb-4">Audio Spectrogram Demo</h1>
-  <Spectrogram />
-</main>
